c6a047d7167c91392f06d0ff1f0043917e5ede7b
Altaha Ansari <altaha.altaha@gmail.com>
2023-12-01 22:19:21 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Altaha Ansari on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
