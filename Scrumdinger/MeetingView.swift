bb90ed24b58f3de66118187ec4e78d3f884973ae
Altaha Ansari <altaha.altaha@gmail.com>
2023-12-02 13:41:57 +0530

    Created Meeting View with Time Data

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Altaha Ansari on 01/12/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+            .accessibilityLabel("Time remaining")
+            .accessibilityLabel("10 Minutes")
+            Circle().strokeBorder(lineWidth: 24)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}){
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next Speaker")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
