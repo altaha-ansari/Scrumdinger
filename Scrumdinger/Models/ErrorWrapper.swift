31fe0766a6fc33a5305f017863448c1b079b5228
Altaha Ansari <altaha.altaha@gmail.com>
2023-12-24 23:05:47 +0530

    Added Error View and Meeting Timer View

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by Altaha Ansari on 20/12/23.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+    
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -10,6 +10,8 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     @StateObject private var store = ScrumStore()
+    @State private var errorWrapper : ErrorWrapper?
+    
     var body: some Scene {
         WindowGroup {
             ScrumsView(scrums: $store.scrums){
@@ -17,17 +19,23 @@ struct ScrumdingerApp: App {
                     do {
                         try await store.save(scrums: store.scrums)
                     } catch {
-                        fatalError(error.localizedDescription)
+                        errorWrapper = ErrorWrapper(error: error, guidance: "Try Again Later")
                     }
                 }
             }
-                .task {
-                    do {
-                        try await store.load()
-                    }catch {
-                        fatalError(error.localizedDescription)
-                    }
+            .task {
+                do {
+                    try await store.load()
+                }catch {
+                    errorWrapper = ErrorWrapper(error: error,
+                                                guidance: "Scrumdinger will load sample data and continue.")
                 }
+            }
+            .sheet(item: $errorWrapper) {
+                    store.scrums = DailyScrum.sampleData
+                } content: { wrapper in
+                    ErrorView(errorWrapper: wrapper)
+            }
         }
     }
 }
