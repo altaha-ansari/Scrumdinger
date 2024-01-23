8ab842c7fa6c64bc1e512af8abff61ef2e74758d
Altaha Ansari <altaha.altaha@gmail.com>
2024-01-23 18:25:16 +0530

    Added History View that shows history of meeting with transcripts

---
+++ Scrumdinger/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  Scrumdinger
+//
+//  Created by Altaha Ansari on 23/01/24.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
